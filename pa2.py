1f9f07be882db0d53f62f9555d3cfd4c69b258d4
HybridRbt <littlejack0120@gmail.com>
2014-05-25 20:06:51 -0500

    index out of range

--- pa2.py
+++ pa2.py
@@ -1,38 +1,39 @@
 __author__ = 'jeredyang'
 
-"""
-Question 1
-GENERAL DIRECTIONS:
-Download the text file here.
+# """
+# Question 1
+# GENERAL DIRECTIONS:
+# Download the text file here.
+#
+# The file contains all of the integers between 1 and 10,000 (inclusive, with no repeats) in unsorted order. The integer
+# in the ith row of the file gives you the ith entry of an input array.
+#
+# Your task is to compute the total number of comparisons used to sort the given input file by QuickSort. As you know,
+# the number of comparisons depends on which elements are chosen as pivots, so we'll ask you to explore three different
+# pivoting rules.
+#
+# Don't count comparison one-by-one. Rather, when there is a recursive call on a subarray of length m, you should simply
+# add m-1 to your running total of comparisons. (this is because the pivot element is compared to each of the other m-1
+# elements in the subarray in this recursive call.)
+#
+# WARNING: The Partition subroutine can be implemented in several different ways, and different implementations can give
+# you differing numbers of comparisons. For this problem, you should implement the Partition subroutine exactly as it is
+# described in the video lectures (otherwise you might get the wrong answer).
+#
+# DIRECTIONS FOR THIS PROBLEM:
+#
+# For the first part of the programming assignment, you should always use the first element of the array as the pivot
+# element.
+#
+# HOW TO GIVE US YOUR ANSWER:
+#
+# Type the numeric answer in the space provided.
+# So if your answer is 1198233847, then just type 1198233847 in the space provided without any space / commas / other
+# punctuation marks. You have 5 attempts to get the correct answer.
+# (We do not require you to submit your code, so feel free to use the programming language of your choice, just type the
+# numeric answer in the following space.)
+# """
 
-The file contains all of the integers between 1 and 10,000 (inclusive, with no repeats) in unsorted order. The integer
-in the ith row of the file gives you the ith entry of an input array.
-
-Your task is to compute the total number of comparisons used to sort the given input file by QuickSort. As you know, the
-number of comparisons depends on which elements are chosen as pivots, so we'll ask you to explore three different
-pivoting rules.
-
-You should not count comparisons one-by-one. Rather, when there is a recursive call on a subarray of length m, you
-should simply add m−1 to your running total of comparisons. (This is because the pivot element is compared to each of
-the other m−1 elements in the subarray in this recursive call.)
-
-WARNING: The Partition subroutine can be implemented in several different ways, and different implementations can give
-you differing numbers of comparisons. For this problem, you should implement the Partition subroutine exactly as it is
-described in the video lectures (otherwise you might get the wrong answer).
-
-DIRECTIONS FOR THIS PROBLEM:
-
-For the first part of the programming assignment, you should always use the first element of the array as the pivot
-element.
-
-HOW TO GIVE US YOUR ANSWER:
-
-Type the numeric answer in the space provided.
-So if your answer is 1198233847, then just type 1198233847 in the space provided without any space / commas / other
-punctuation marks. You have 5 attempts to get the correct answer.
-(We do not require you to submit your code, so feel free to use the programming language of your choice, just type the
-numeric answer in the following space.)
-"""
 
 def read_file(filename):
     f = open(filename)
@@ -51,7 +52,7 @@ def partition(a, p, l):
     i = p + 1
     #start point of i as the lower bound of partition
 
-    for j in range(p + 1, l + 1):
+    for j in range(p + 1, l):
         if a[j] < a[p]:
             swap(a, j, i)
             i += 1
@@ -71,7 +72,13 @@ def swap(a, x, y):
 
     return a
 
-# ls = read_file("testFile.txt")
+
+def test_partition():
+    ls = read_file("testFile1.txt")
+    print partition(ls, 1, len(ls))
+
+test_partition()
+
 # print ls[: len(ls) / 2]
 # print ls[len(ls) / 2:]
 # test_sort_count("testFile1.txt")
