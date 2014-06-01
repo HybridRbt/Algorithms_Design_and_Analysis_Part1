b7957de26c2420a66531fae397e40a906794c0ce
HybridRbt <littlejack0120@gmail.com>
2014-05-31 23:34:56 -0500

    main min_cut() function and test

--- pa3.py
+++ pa3.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 __author__ = 'jeredyang'
 
@@ -87,7 +88,7 @@ class EdgeList:
     def is_in(self, edge):
         assert isinstance(edge, Edge)
         if len(self.egl) == 0:
-            return False   # empty list
+            return False  # empty list
 
         for each_edge in self.egl:
             if each_edge.rev(edge):  # same edge with reversed sp & ep
@@ -97,10 +98,35 @@ class EdgeList:
 
         return False
 
-    def add_eg(self, edge):
+    def add_eg(self, edge, allow_dup):
         assert isinstance(edge, Edge)
-        if not self.is_in(edge):
+        if allow_dup:
             self.egl.append(edge)
+        else:
+            if not self.is_in(edge):
+                self.egl.append(edge)
+
+    def del_eg(self, edge):
+        assert isinstance(edge, Edge)
+        assert edge in self.egl
+
+        self.egl.remove(edge)
+        return self.egl
+
+    def get_edge(self, sp):
+        edge_ls = []
+        for each_edge in self.egl:
+            if each_edge.get_sp() == sp or each_edge.get_ep() == sp:
+                edge_ls.append(each_edge)
+
+        return edge_ls
+
+    def clean(self):
+        for each_edge in self.egl:
+            if each_edge.is_self_loop():
+                self.egl.remove(each_edge)
+
+        return self.egl
 
     def op(self):  # present itself as a group of points (string)
         egs = []
@@ -126,7 +152,7 @@ class Graph:
 
     def add_eg(self, sp, ep):
         edg = Edge(sp, ep)
-        self.egs.add_eg(edg)  
+        self.egs.add_eg(edg)
 
     def add_v(self, p):
         if not p in self.vts:
@@ -135,48 +161,68 @@ class Graph:
     def get_eg_list(self):
         return self.egs
 
-    def set_sp(self, sp):
-        self.p1 = sp
+    def pt_eg_list(self):
+        return self.egs.op()
 
-    def set_ep(self, ep):
-        self.p2 = ep
+    def get_v_list(self):
+        return self.vts
 
-    def is_self_loop(self):
-        return self.p1 == self.p2
-
-    def equal(self, edge):
-        assert isinstance(self.p1, Edge)
-        return self.p1 == edge.get_ep() and self.p2 == edge.get_sp()
-        # reversed edge in an undirected graph are considered the same
-
-    def op(self):  # present itself as a group of points (string)
-        eg = "(" + str(self.p1) + ", " + str(self.p2) + ")"
-        return eg
+    def contraction(self, edge):
+        self.egs.del_eg(edge)
+        self.vts.remove(edge.get_sp())
+        for each_edge in self.egs.get_edge(edge.get_sp()):
+            new_sp = edge.get_ep()
+            new_ep = each_edge.get_ep()
+            new_ed = Edge(new_sp, new_ep)
+            self.egs.add_eg(new_ed)
 
 
 # create graph from given list
-def create_graph(list):
-    graph = []  # graph is a list of edges
-    for each_line in list:
+def create_graph(ls):
+    gr = Graph()  # graph is a list of edges
+    for each_line in ls:
         sp = each_line[0]  # start point is line[0]
+        gr.add_v(sp)
         for ep_index in range(1, len(each_line)):  # for each item after line[0] in each line, create an edge
-            new_edge = Edge(sp, each_line[ep_index])
-            graph.append(new_edge)
+            gr.add_eg(sp, each_line[ep_index])
+            gr.add_v(each_line[ep_index])
 
-    return graph
-
-ed = Edge(1, 2)
-ed1 = Edge(3, 4)
-edl = EdgeList()
-edl.add_eg(ed)
-edl.add_eg(ed)
+    return gr
 
 
-print edl.op()
+def min_cut(graph):
+    while len(graph.get_eg_list().get_list()) > 2:
+        ran_v = random.choice(graph.get_v_list())
+        ran_edge_list = graph.get_eg_list().get_edge(ran_v)
+        ran_edge = random.choice(ran_edge_list)
+        graph.contraction(ran_edge)
+        graph.get_eg_list().clean()
+
+    return len(graph.get_eg_list())
 
 
+# as long as len(graph.v_list) > 2:
+#   randomly choose an edge:
+#     randomly choose a v:
+#         randomly choose an edge start from v:
+#  graph.contraction(edge)
+#  graph.get_eg_list().clean()
+#
+# return len(graph.get_eg_list())
+
+def test():
+    fn = "simple.txt"
+    lst = read_csv(fn)
+    print lst
+    gra = create_graph(lst)
+    print gra.pt_eg_list()
+    print gra.get_v_list()
+
+    minct = min_cut(gra)
+    print minct
 
 
+test()
 
 
 
